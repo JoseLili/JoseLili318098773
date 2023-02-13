189e6e64df00ef4f87d79e76775b3aac46210502
José Lili <68255530+JoseLili@users.noreply.github.com>
2023-02-13 12:38:21 -0600

    Quitamos las variables
    Esto probablemente lo haria un dev junio

--- suma.java
+++ suma.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Suma {
 
    public static void main( String[] args ) {
-      int a, b, c = 0;
+
       Scanner s = new Scanner( System.in );
       System.out.println( "Introduce 3 números: ");
       a = s.nextInt();
