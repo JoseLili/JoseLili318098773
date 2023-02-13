c73dee5e349e47c392b7ddc6e147f4ae400be26f
José Lili <68255530+JoseLili@users.noreply.github.com>
2023-02-13 12:34:34 -0600

    Agrego otra variable
    Se agrego otra variable, donde ahora suma 3 numero en vez de 2

--- suma.java
+++ suma.java
@@ -5,11 +5,13 @@ class Suma {
    public static void main( String[] args ) {
       int a, b, c = 0;
       Scanner s = new Scanner( System.in );
-      System.out.println( "Introduce dos números: ");
+      System.out.println( "Introduce 3 números: ");
       a = s.nextInt();
       b = s.nextInt();
-      c = a + b;
+      d = s.nextInt();
+      c = a + b + b;
       System.out.println( "La suma de a y b es: " + c );
+      
    }
 
 }
